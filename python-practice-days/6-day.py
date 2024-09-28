e43eace642197ff527484908596e86c7073f90b4
shafeeshafee <shafeelinks@gmail.com>
2024-09-28 05:55:59 -0400

    complete day 6

---
+++ python-practice-days/6-day.py
@@ -0,0 +1,20 @@
+students = [
+    {
+        "name": "Shafee",
+        "age": 27,
+        "grade": 2.2
+    },
+    {
+        "name": "Clint",
+        "age": 29,
+        "grade": 3.6 
+    },
+    {
+        "name": "Jordon",
+        "age": 25,
+        "grade": 4.0
+    }   
+]
+
+for student in students:
+    print(f"Student: {student["name"]}\nAge: {student["age"]}\nGrade: {student["grade"]}\n")
